1bb9182ef958ecf88b91fcf65e09b6856bd94c22
Gohand <Andy.Notfound@gmail.com>
2025-02-23 23:27:41 +0700

    feat(home): hide carousel arrow on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
     import { reactive } from 'vue';
+    import { useIsMobile } from '#imports';
+
+    const { isMobile } = useIsMobile()
 
     const recommendations = reactive([
         {
@@ -60,9 +63,17 @@
                         <card-landscape />
                     </CarouselItem>
                 </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
+                <CarouselPrevious v-if="!isMobile" />
+                <CarouselNext v-if="!isMobile" />
             </Carousel>
+        </section>
+        <section class="section">
+            <section-head
+                title="Costumer Reviews"
+                desc="See What Our Fellow Travelers Have to Say About Us"
+                desc-class="w-full md:w-[521px] text-center"
+                container-class="mt-28 mb-6 flex flex-col items-center"
+            />
         </section>
     </div>
 </template>
